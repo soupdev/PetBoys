a742542b353b877aa6c8ad259d74c7b59aca46a0
Chelsea Campbell <ccampbell@ezprints.com>
2020-05-28 01:15:25 -0400

    Updates
    Updated layout, 86 dec, added attributes, linked button,
    
    needs if statements for icons, incons

--- src/js/main.js
+++ src/js/main.js
@@ -20,15 +20,23 @@ let showDogs = function(){
         let dog_name = dog_file[i].name;
         let dog_desc = dog_file[i].description;
         let dog_gender = dog_file[i].gender;
+
+        let dog_size = dog_file[i].size;
+        let dog_url = dog_file[i].url;
+        let dog_breed = dog_file[i].breeds.primary;
+        
+
+
+
         let dog_photos = dog_file[i].primary_photo_cropped;
         //console.log (dog_photos);
-        let sm_dog_photo;
+        let md_dog_photo;
         //this is an array of photos per dog, some arrays are null
         if(dog_photos !=null){ //if the array is not empty
-            sm_dog_photo = dog_photos.small;
+            md_dog_photo = dog_photos.medium;
             //console.log(sm_dog_photo);
         } else{ //if array is empty, img src is default img
-            sm_dog_photo = "dist/img/dog-heads.png";
+            md_dog_photo = "dist/img/dog_comingSoon2.jpg";
         }
        
         //each pet has a file (adoption card), each card has text section (adoption info) and picture section (adoption photo.
@@ -45,46 +53,101 @@ let showDogs = function(){
        //this div holds the text information about each dog
         var pet_info = document.createElement("div"); 
         pet_info.setAttribute("class","pet-info")
+
+        //this div holds the pet icons about each dog
+        var pet_icons = document.createElement("div"); 
+        pet_icons.setAttribute("class","pet-icons");
        
-        //this div holds the photo of each dog
-        var adoption_photo = document.createElement("div");
-        adoption_photo.setAttribute("class","adoption-photo");
+        //this div holds the photo-container of each dog
+        var adoption_photo_container = document.createElement("div");
+        adoption_photo_container.setAttribute("class","adoption-photo-container");
         
 
         //create elements and add class name
         var pet_name = document.createElement("h3");
         pet_name.setAttribute("class","pet-name");
-
+        
         var pet_desc = document.createElement("p");
         pet_desc.setAttribute("class","pet-desc");
+        
+        var pet_gender = document.createElement("p");
+        pet_gender.setAttribute("class" , "pet_gender");
+
+        var pet_size = document.createElement("p");
+        pet_size.setAttribute("class" , "pet_size");
+
+        var pet_breed = document.createElement("p");
+        pet_breed.setAttribute("class" , "pet_breed");
+
+        var pet_icon1 = document.createElement("p");
+        pet_icon1.setAttribute("class", "icon"); 
+        var pet_icon2 = document.createElement("p")
+        pet_icon2.setAttribute("class", "icon");
+        var pet_icon3 = document.createElement("p")
+        pet_icon3.setAttribute("class", "icon");
+        var pet_icon4 = document.createElement("p")
+        pet_icon4.setAttribute("class", "icon");
+
+        var pet_url = document.createElement("a");
+        pet_url.setAttribute("class", "pet-url");
+        pet_url.href= dog_url;
 
         var adopt_button = document.createElement("button");
 
         var pet_photo = document.createElement("img");
         pet_photo.setAttribute("class","pet-img");
-        pet_photo.src= `${sm_dog_photo}`;
+        pet_photo.src= `${md_dog_photo}`;
         
 
         //add inner data to elements 
+        adopt_button.innerText = 'Learn More About ' + dog_name;
         pet_name.innerText = dog_name; 
-        pet_desc.innerHTML =  dog_desc + "<br>" + "Gender: " + dog_gender;
-        adopt_button.innerText = 'Learn More'
+        pet_gender.innerText = "Gender: " + dog_gender;
+        pet_size.innerText = "Size: " + dog_size;
+        pet_breed.innerText = "Breed: " + dog_breed;
+
+        pet_icon1.innerText = "Housebroken :";
+        pet_icon2.innerText = "Spayed/Neutered :";
+        pet_icon3.innerText = "Vaccinations :";
+        pet_icon4.innerText = "Special Needs: ";
+        
         
         //add text divs to pet_info 
+        pet_desc.appendChild(pet_gender);
+        pet_desc.appendChild(pet_size);
+        pet_desc.appendChild(pet_breed);
+
+        //add icon divs to pet_icons
+        pet_icons.appendChild(pet_icon1);
+        pet_icons.appendChild(pet_icon2);
+        pet_icons.appendChild(pet_icon3);
+        pet_icons.appendChild(pet_icon4);
+
+        //wrap button in <a> tag 
+        pet_url.appendChild(adopt_button);
+
+        //add major divs to div container
         pet_info.appendChild(pet_name);
         pet_info.appendChild(pet_desc);
-        pet_info.appendChild(adopt_button);
+        pet_info.appendChild(pet_icons)
+
+      
+
+
+        //pet_info.appendChild(adopt_button);
+        pet_info.appendChild(pet_url);
+        
         
 
         //add all pet_info to adoption_info
         adoption_info.appendChild(pet_info);
 
         //add img to adoption_photo
-        adoption_photo.appendChild(pet_photo);
+        adoption_photo_container.appendChild(pet_photo);
 
         //add all adoption_info and adoption_photo to adoption_file
         adoption_file.appendChild(adoption_info);
-        adoption_file.appendChild(adoption_photo);
+        adoption_file.appendChild(adoption_photo_container);
        
         //add adoption card to HTML document
         adoptions.appendChild(adoption_file);
